fe5b4c92a7def3b297bf1d2b35007e33d465b46d
katussska <katkabaier@gmail.com>
2024-11-27 23:48:01 +0100

    details tree and chat
    Took 14 minutes

--- src/components/chat/ChatContent.tsx
+++ src/components/chat/ChatContent.tsx
@@ -1,6 +1,6 @@
 export default function ChatContent() {
   return (
-    <div className="flex-grow content-end pb-10">
+    <div className="mb-10 mt-1 w-full max-w-4xl flex-grow content-end bg-white">
       <p>//messages</p>
     </div>
   );
--- src/components/tree/TreeSection.tsx
+++ src/components/tree/TreeSection.tsx
@@ -9,7 +9,7 @@ export default function TreeSection() {
 
   return (
     <div className="flex h-screen w-full flex-1 bg-sidebar">
-      <div className="border-gray-30 m-4 mt-20 flex w-full flex-grow flex-col items-center rounded-2xl border">
+      <div className="border-gray-30 mx-4 mb-7 mt-20 flex w-full flex-grow flex-col items-center rounded-2xl border">
         <h1 className="flex-grow content-center pb-10">Conversation Tree</h1>
         <p className="mb-7 flex w-full flex-row items-center justify-center">scales</p>
       </div>
